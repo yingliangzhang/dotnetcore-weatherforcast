5beba8748fec07e3165337c848a1806e1eb6d08e
Leon Zhang <yingliangzhang89@gmail.com>
2019-12-02 23:12:01 +1100

    wip: creating service tests

---
+++ WeatherForecastStation.Service.Test/WeatherForecastServiceTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace WeatherForecastStation.Service.Test
+{
+    public class WeatherForecastServiceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
