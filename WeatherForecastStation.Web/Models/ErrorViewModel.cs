d49425ef2c91f90642c45747fbd4a888291ffdd2
Leon Zhang <yingliangzhang89@gmail.com>
2019-12-02 22:06:30 +1100

    refactor: moved web project into folder, created solution file

---
+++ WeatherForecastStation.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WeatherForecastStation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
